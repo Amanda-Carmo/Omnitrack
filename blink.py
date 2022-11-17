5eb5e2f87a562d3d9d8f212d225a648e6907d0e7
Amanda Carmo <amanda.rosa.ismart@gmail.com>
2022-11-17 03:07:33 -0300

    blinks and direction

---
+++ blink.py
@@ -0,0 +1,42 @@
+import math
+
+# Euclaidean distance 
+def euclaideanDistance(point, point1):
+    x, y = point
+    x1, y1 = point1
+    distance = math.sqrt((x1 - x)**2 + (y1 - y)**2)
+    return distance
+
+# Blinking Ratio
+def blinkRatio(img, landmarks, right_indices, left_indices):
+    # Right eyes 
+    # horizontal line 
+    rh_right = landmarks[right_indices[0]]
+    rh_left = landmarks[right_indices[8]]
+    # vertical line 
+    rv_top = landmarks[right_indices[12]]
+    rv_bottom = landmarks[right_indices[4]]
+    # draw lines on right eyes 
+    # cv.line(img, rh_right, rh_left, utils.GREEN, 2)
+    # cv.line(img, rv_top, rv_bottom, utils.WHITE, 2)
+
+    # LEFT_EYE 
+    # horizontal line 
+    lh_right = landmarks[left_indices[0]]
+    lh_left = landmarks[left_indices[8]]
+
+    # vertical line 
+    lv_top = landmarks[left_indices[12]]
+    lv_bottom = landmarks[left_indices[4]]
+
+    rhDistance = euclaideanDistance(rh_right, rh_left)
+    rvDistance = euclaideanDistance(rv_top, rv_bottom)
+
+    lvDistance = euclaideanDistance(lv_top, lv_bottom)
+    lhDistance = euclaideanDistance(lh_right, lh_left)
+
+    reRatio = rhDistance/rvDistance
+    leRatio = lhDistance/lvDistance
+
+    ratio = (reRatio+leRatio)/2
+    return ratio 
---
+++ position.py
@@ -0,0 +1,72 @@
+# Function reference: 
+# https://github.com/Asadullah-Dal17/Eyes-Position-Estimator-Mediapipe.git
+
+import cv2 as cv
+import numpy as np
+import math
+
+# colors 
+# values =(blue, green, red) opencv accepts BGR values not RGB
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+YELLOW =(0,255,255)
+GRAY = (128,128,128)
+GREEN = (0,255,0)
+ORANGE = (0,165,255)
+PINK = (147,20,255)
+
+# Pixel counter
+def pixel_count(f_piece, s_piece, t_piece):
+    # counting black pixel in each part 
+    right_part = np.sum(f_piece==0)
+    center_part = np.sum(s_piece==0)
+    left_part = np.sum(t_piece==0)
+    # creating list of these values
+    eye_parts = [right_part, center_part, left_part]
+
+    # getting the index of max values in the list 
+    max_index = eye_parts.index(max(eye_parts))
+    pos_eye ='' 
+    
+    # Show in screen 
+    if max_index==0:
+        pos_eye="RIGHT"
+        color=[BLACK, GREEN]
+    elif max_index == 1:
+        pos_eye = 'CENTER'
+        color = [YELLOW, PINK]
+    elif max_index == 2:
+        pos_eye = 'LEFT'
+        color = [GRAY, YELLOW]
+    else:
+        pos_eye="Closed"
+        color = [GRAY, YELLOW]
+    
+    return pos_eye, color
+
+
+def position(eye_mask):
+    # get the height and width of the eye
+    h, w, z = eye_mask.shape
+
+    # remove noise from eye image
+    gaussian_blur = cv.GaussianBlur(eye_mask, (9,9),0)
+    median_blur = cv.medianBlur(gaussian_blur, 3)
+
+    # threshold to convert binary image
+    ret, threshed_eye = cv.threshold(median_blur, 130, 255, cv.THRESH_BINARY)
+
+    # create fixd part for eye with 
+    piece = int(w/3) 
+
+    # slicing the eyes into three parts 
+    right_piece = threshed_eye[0:h, 0:piece]
+    center_piece = threshed_eye[0:h, piece:(piece+piece)]
+    left_piece = threshed_eye[0:h, (piece + piece):w]
+    
+    # calling pixel counter function
+    eye_position, color = pixel_count(right_piece, center_piece, left_piece)
+    
+    return eye_position, color
+
+
